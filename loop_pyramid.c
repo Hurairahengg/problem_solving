6bc3ee7ca7401c9a3dc77404e482ca62d7666cdf
Hurairahengg <69149863+Hurairahengg@users.noreply.github.com>
2021-02-08 16:35:29 +0600

    Half Pyramid With Loop...
    Cannnont create a pyramid ?
    Here is a way to make a half pyramid with For loop.....

---
+++ loop_pyramid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+
+
+        for(int i = 1;i <= 10;++i){     //The loop will be start here....
+            for(int j = 1;j <= i ;++j){    //This one help to print stars
+                printf("* ");
+
+            }
+                printf("\n");   //New line helps to make the Pyramid Shape.
+        }
+}
+//Thanks for reading
